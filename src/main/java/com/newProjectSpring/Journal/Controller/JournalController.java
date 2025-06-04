c5a0fd55aebc294215a2d4d82325e29a214ffafe
Ankur Karna <32448296+Ankur-karna@users.noreply.github.com>
2025-06-04 20:30:50 +0530

    Journal application using spring boot

--- src/main/java/com/newProjectSpring/Journal/Controller/JournalController.java
+++ src/main/java/com/newProjectSpring/Journal/Controller/JournalController.java
@@ -6,24 +6,44 @@ import org.springframework.web.bind.annotation.*;
 import java.util.*;
 
 @RestController
-@RequestMapping("entry")
+@RequestMapping("journal")
 public class JournalController {
 
     private final Map<Long, JournalData> journalEntries = new HashMap<>();
 
 
+
+
+    //to get all journal entries
     @GetMapping
-    public List<JournalData> showAll(){
+    public List<JournalData> getAllJournal(){
         return new ArrayList<>(journalEntries.values());
     }
 
+    // find journal entries by id
+    @GetMapping("id/{myId}")
+    public JournalData updateEntry(@PathVariable Long myId){
+        return journalEntries.get(myId);
+
+    }
+    // create new journal entries
     @PostMapping
     public boolean addNewEntry(@RequestBody JournalData myData){
         journalEntries.put(myData.getId(), myData);
         return true;
     }
 
-    public void updateEntry(){
 
+    //update entry by id
+    @PutMapping("/id/{myId}")
+    public JournalData updateJournalEntries(@PathVariable Long myId, @RequestBody JournalData updateData){
+         return journalEntries.put(myId,updateData);
+    }
+
+    // delete entries by id
+    @DeleteMapping("id/{myId}")
+    public boolean deleteJournalEntries(@RequestBody Long myId){
+        journalEntries.remove(myId);
+        return true;
     }
 }
